287dec2f4752c2e173caf8d68f8f7ccbae192847
Aamir-Sohail772 <aamirsohail772@gmail.com>
2023-10-14 16:04:33 +0530

    Renaming of file

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -3,7 +3,7 @@ import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
 import { extendTheme } from "@chakra-ui/react";
-import { Button } from "./button";
+import { Button } from "./Button";
 
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
